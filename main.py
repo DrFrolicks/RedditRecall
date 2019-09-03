f6d1186d874219dc676a13a03aa0ed712fe2ce96
Evan Cheng <dr.frolicks@gmail.com>
2019-09-02 18:02:01 -0700

    shows full title, opens post when prompted

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import praw
-from nltk.corpus import wordnet as wn
+import webbrowser
+import random
+from nltk.tag import pos_tag
 
-nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}
+#nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}
 
 #returns every three words of string
 word_interval = 3
 def fragment(complete_text: str) -> str: 
-    all_words = complete_text.split()
+    tagged_words = pos_tag(complete_text.split())
+    nouns = [word for word,pos in tagged_words if pos == 'NNP' or pos == 'NN']
 
-    return [word for word in all_words if word in nouns]
+    return nouns
     # returned_words = []
     # for i in range(0, len(all_words) - len(all_words) % word_interval, word_interval):
     #     returned_words.append(all_words[i])
@@ -16,9 +19,23 @@ def fragment(complete_text: str) -> str:
 
 
 
+
 reddit = praw.Reddit('MyCredentials', user_agent="user_agent=Reddit Recall 1.0 by /u/Frolicks https://github.com/DrFrolicks/RedditRecall")
 
-titles = []
-for post in reddit.user.me().upvoted(limit=10): 
-    if(post.over_18):
-        print(fragment(post.title))
+submissions = [] 
+
+for submission in reddit.user.me().upvoted(limit=500): 
+    if(not submission.over_18):
+        submissions.append(submission)
+
+random.shuffle(submissions)
+
+for submission in submissions:
+    print(fragment(submission.title))
+    input()
+    print(submission.title)
+    if (input("Open post? (y/n)") == 'y'):
+        webbrowser.open(submission.url)
+
+
+        
